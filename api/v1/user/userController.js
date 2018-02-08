26d417998ee085ab38b9e9b76a78b3faca12e739
Rick <rick.holterman.rh@gmail.com>
2018-02-08 20:45:10 +0100

    add some validation for registration

--- api/v1/user/userController.js
+++ api/v1/user/userController.js
@@ -16,21 +16,47 @@ var sendJSONresponse = function(res, status, content) {
 
 module.exports.register = function(req, res) {
 
-    var user = new User();
+    var response; // JSON to send as response
+    var status; // HTTP status code to return
 
-    user.name = req.body.name;
-    user.email = req.body.email;
+    if (
+        req.body.name == null || req.body.name == "" || 
+        req.body.email == null || req.body.email == "" ||
+        req.body.password == null || req.body.password == ""
+    ) {
+        status = 405;
+        response = { "error": "We missen nog wat informatie! Weet je zeker dat je alles ingevuld hebt?" };
 
-    user.setPassword(req.body.password);
+        res.status(status);
+        res.json(response);
+    } else {
+        var user = new User();
 
-    user.save(function(err) {
-        var token;
-        token = user.generateJwt();
-        res.status(200);
-        res.json({
-            "token" : token
+        user.name = req.body.name;
+        user.email = req.body.email;
+
+        user.setPassword(req.body.password);
+
+        user.save(function(err) {
+
+            // TODO: add more validation (password not good enough etc.)
+            if (err) {
+                if (err.code == 11000) {
+                    response = { "error": "Er bestaat al een Klasse Reporter met het e-mailadres dat je hebt ingevuld!" };
+                } else {
+                    response = { "error": "Er is iets fout gegaan." };
+                }
+                status = 400;
+            } else {
+                var token;
+                token = user.generateJwt();
+                response = { "token": token };
+                status = 200;
+            }
+            res.status(status);
+            res.json(response);
         });
-    });
+    } 
 };
 
 module.exports.login = function(req, res) {
